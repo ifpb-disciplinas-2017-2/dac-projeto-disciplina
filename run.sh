61c455f9bca419b88e7e136c53fe3062512e11ca
Rodrigo Bento <rodrigobentor2014@gmail.com>
2017-09-30 17:16:28 -0300

    Alteracao no run.sh e criacao do executavel kill

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sudo docker stop dac
 sudo docker rm dac
 mvn clean install
-sudo docker rmi projeto/app
-sudo docker build -t projeto/app .
-sudo docker run -p 8080:8080 -p 3700:3700 -d --name dac projeto/app
+sudo docker rmi projeto/web
+sudo docker build -t projeto/web .
+sudo docker run -p 8080:8080 -p 3700:3700 -d --name dac projeto/web
