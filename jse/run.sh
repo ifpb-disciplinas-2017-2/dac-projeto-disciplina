bce03206ea809a25628e46a8adf7690ed0189eb8
Rodrigo Bento <rodrigobentor2014@gmail.com>
2017-10-16 14:45:44 -0300

    Modulo JSE removido

--- jse/run.sh
+++
@@ -1,6 +0,0 @@
-sudo docker stop jse
-sudo docker rm jse 
-sudo docker rmi projeto/jse
-sudo docker build -t projeto/jse .
-sudo docker run -d --name jse --link dac:host-core projeto/jse
-sudo docker logs -f jse
