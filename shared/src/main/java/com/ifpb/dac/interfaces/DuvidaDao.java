94b879d2afe99fd18bfaabca7419b667efc58cd4
natansevero <natanseverolp@gmail.com>
2017-10-17 01:21:39 -0200

    Interface do dao de duvidas

---
+++ shared/src/main/java/com/ifpb/dac/interfaces/DuvidaDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifpb.dac.interfaces;
+
+import com.ifpb.dac.entidades.Duvida;
+import com.ifpb.dac.entidades.Professor;
+import java.util.List;
+
+/**
+ *
+ * @author natan
+ */
+public interface DuvidaDao {
+    void adicionar(Duvida duavida);
+    void remover(Duvida duvida);
+    List<Duvida> listarTodos();
+    Duvida buscarPorId(int id);
+    List<Duvida> buscarPorProfessor(Professor professor);
+}
